e72236dd8f4c6d9acfdd1ddb0161718702783eab
Guilherme <guilherme.oliveira182@yahoo.com.br>
2020-04-11 15:34:46 -0300

    Termino modulo Node

---
+++ Node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloa')
+const moudloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moudloB.bomDia)
+console.log(moudloB.boaNoite())
+console.log(moudloB)
